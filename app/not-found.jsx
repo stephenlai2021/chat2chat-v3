fcdcbd7d92848a5638b8baf6f769c2f46757a89e
Stephen_Lai <stephenlai2021@gmail.com>
2024-03-04 19:46:30 +0800

    fix not-found page ui

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
-
 export default function NotFound() {
   return (
     <div className="flex items-center justify-center h-screen w-full">
       <div>
-        <h2 className="text-3xl">There was a problem.</h2>
-        <p>We could not find the page you were looking for.</p>
+        <h1 className="text-center"></h1>
+        <p>We could not find the page you were looking for. 😅</p>
       </div>
     </div>
   );
